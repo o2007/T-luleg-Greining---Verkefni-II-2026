83ac181942f9afd514296b6a792898270d149feb
helgimednis <helgimednis@gmail.com>
2026-03-27 19:43:30 +0000

    Hluti 2

--- hluti2.m
+++ hluti2.m
@@ -1,22 +1,38 @@
-clear; clc; close all;
+clear; clc;
 
-hradi = @(t) sqrt((0.3 + 7.8*t - 14.1*t.^2).^2 + (0.3 + 1.8*t - 8.1*t.^2).^2);
+hradi = @(t) sqrt((0.3 + 7.8*t - 14.1*t.^2).^2 + ...
+                  (0.3 + 1.8*t - 8.1*t.^2).^2);
 
 quadTol = 1e-8;
 bisectTol = 1e-8;
 s = 0.5;
 
-totalLen = adaptQuad(hradi, 0, 1, quadTol);
+L = adaptQuad(hradi, 0, 1, quadTol);
 
 klukka = tic;
-tStjarna = bisectT(hradi, s, totalLen, quadTol, bisectTol);
-timiBisect = toc(klukka);
+tStjarna = bisectT(hradi, s, L, quadTol, bisectTol);
+timiBisect2 = toc(klukka);
 
-disp(' ');
-disp('Liður 2:');
-disp(' ');
+disp('');
+disp('Hluti 2:');
+disp('');
 fprintf('t* = %.3f\n', tStjarna);
-fprintf('Keyrslutími = %.6f s\n', timiBisect);
+fprintf('Keyrslutimi = %.6f s\n', timiBisect2);
+
+
+function I = adaptQuad(f, a, b, tol)
+    c = (a + b)/2;
+
+    heilt = (b - a) * (f(a) + f(b)) / 2;
+    vinstri = (c - a) * (f(a) + f(c)) / 2;
+    haegri = (b - c) * (f(c) + f(b)) / 2;
+
+    if abs(heilt - (vinstri + haegri)) < 3*tol
+        I = vinstri + haegri;
+    else
+        I = adaptQuad(f, a, c, tol/2) + adaptQuad(f, c, b, tol/2);
+    end
+end
 
 
 function tStjarna = bisectT(f, s, totalLen, quadTol, tol)
@@ -30,13 +46,13 @@ function tStjarna = bisectT(f, s, totalLen, quadTol, tol)
 
     a = 0;
     b = 1;
-    fa = adaptQuad(f, 0, a, quadTol) - s*totalLen;
+    fa = -s * totalLen;
 
-    while (b-a)/2 > tol
-        c = (a+b)/2;
-        fc = adaptQuad(f, 0, c, quadTol) - s*totalLen;
+    while (b - a)/2 > tol
+        c = (a + b)/2;
+        fc = adaptQuad(f, 0, c, quadTol) - s * totalLen;
 
-        if fa*fc < 0
+        if fa * fc < 0
             b = c;
         else
             a = c;
@@ -44,20 +60,5 @@ function tStjarna = bisectT(f, s, totalLen, quadTol, tol)
         end
     end
 
-    tStjarna = (a+b)/2;
-end
-
-
-function I = adaptQuad(f, a, b, tol)
-    c = (a+b)/2;
-
-    allt = (b-a)*(f(a)+f(b))/2;
-    vinstri = (c-a)*(f(a)+f(c))/2;
-    haegri = (b-c)*(f(c)+f(b))/2;
-
-    if abs(allt - (vinstri + haegri)) < 3*tol
-        I = vinstri + haegri;
-    else
-        I = adaptQuad(f, a, c, tol/2) + adaptQuad(f, c, b, tol/2);
-    end
+    tStjarna = (a + b)/2;
 end
