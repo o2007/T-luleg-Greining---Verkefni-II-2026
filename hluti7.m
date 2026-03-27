67e1928c461d0fa6df94ccc9be382e463f218293
helgimednis <helgimednis@gmail.com>
2026-03-27 18:59:03 +0000

    Breyting Hluti 7

--- hluti7.m
+++ hluti7.m
@@ -1,89 +1,97 @@
+% Hluti 7 
+clear; clc;
+disp('');
+disp('Lidur 7:');
+disp('');
+
+% Ferillinn
 x = @(t) 0.5 + 0.3*t + 3.9*t.^2 - 4.7*t.^3;
 y = @(t) 1.5 + 0.3*t + 0.9*t.^2 - 2.7*t.^3;
- 
+
+% Hradi ferilsins
 hradi = @(t) sqrt((0.3 + 7.8*t - 14.1*t.^2).^2 + ...
                   (0.3 + 1.8*t - 8.1*t.^2).^2);
- 
+
+% Vikmork og fjoldi ramma
 quadTol = 1e-8;
-nFrames = 200;
- 
+fjoldiRamma = 200;
+
+% Heildarbogalengd
 L = adaptQuad(hradi, 0, 1, quadTol);
- 
+
+% Punktar til ad teikna ferilinn
 tPlot = linspace(0, 1, 1000);
-xFull = x(tPlot);
-yFull = y(tPlot);
-sVals = linspace(0, 1, nFrames);
- 
-% Progress-kúrfur og nöfn
-progressKurfur = { @(s) s,                              'C(s) = s (jafn hraði)';
-                   @(s) s.^(1/3),                       'C(s) = s^{1/3}';
-                   @(s) s.^2,                           'C(s) = s^2';
-                   @(s) sin(s*pi/2),                    'C(s) = sin(s\pi/2)';
-                   @(s) 0.5 + 0.5*sin((2*s-1)*pi/2),   'C(s) = ease-in-out' };
- 
-for i = 1:size(progressKurfur, 1)
-    C    = progressKurfur{i, 1};
-    nafn = progressKurfur{i, 2};
- 
-    [xBall, yBall] = progressPositions(x, y, hradi, L, C, nFrames, quadTol);
- 
-    cVals = C(sVals);
-    titlar = arrayfun(@(k) sprintf('%s:  s = %.3f,  C(s) = %.3f', nafn, sVals(k), cVals(k)), ...
-                      1:nFrames, 'UniformOutput', false);
- 
-    animateFerill(xFull, yFull, xBall, yBall, 'r', titlar);
+xFerill = x(tPlot);
+yFerill = y(tPlot);
+sGildi = linspace(0, 1, fjoldiRamma);
+
+% Stodufoll C(s) og nofn þeirra
+stodufoll = {
+    @(s) s,                            'C(s) = s (jafn hradi)';
+    @(s) s.^(1/3),                     'C(s) = s^(1/3)';
+    @(s) s.^2,                         'C(s) = s^2';
+    @(s) sin(s*pi/2),                  'C(s) = sin(pi*s/2)';
+    @(s) 0.5 + 0.5*sin((2*s-1)*pi/2), 'C(s) = ease-in-out'
+};
+
+% Keyra hreyfimynd fyrir hvert stodufall
+for i = 1:size(stodufoll, 1)
+    C = stodufoll{i, 1};
+    nafn = stodufoll{i, 2};
+
+    [xPunktur, yPunktur] = finnaStadsetningarC( ...
+        x, y, hradi, L, C, fjoldiRamma, quadTol);
+
+    cGildi = C(sGildi);
+    titlar = arrayfun(@(k) sprintf('%s: s = %.3f, C(s) = %.3f', ...
+                      nafn, sGildi(k), cGildi(k)), ...
+                      1:fjoldiRamma, 'UniformOutput', false);
+
+    hreyfaFeril(xFerill, yFerill, xPunktur, yPunktur, 'r', titlar);
     pause(1);
 end
 
-function I = adaptQuad(f,a,b,tol)
-    c = (a+b)/2;
 
-    allt = (b-a)*(f(a)+f(b))/2;
-    vinstri  = (c-a)*(f(a)+f(c))/2;
-    haegri = (b-c)*(f(c)+f(b))/2;
+function [xPunktur, yPunktur, tStjarna] = finnaStadsetningarC( ...
+    x, y, hradi, L, C, fjoldiRamma, quadTol)
 
-    if abs(allt - (vinstri + haegri)) < 3*tol
-        I = vinstri + haegri;
-    else
-        I = adaptQuad(f,a,c,tol/2) + adaptQuad(f,c,b,tol/2);
-    end
-end
+    sGildi = linspace(0, 1, fjoldiRamma);
+    cGildi = C(sGildi);
+    tStjarna = zeros(1, fjoldiRamma);
 
-function [xBall, yBall, tStars] = progressPositions(x, y, hradi, L, C, nFrames, quadTol)
-    sVals = linspace(0, 1, nFrames);
-    cVals = C(sVals);
-    tStars = zeros(1, nFrames);
-
-    for k = 1:nFrames
-        if cVals(k) <= 0
-            tStars(k) = 0;
-        elseif cVals(k) >= 1
-            tStars(k) = 1;
+    for k = 1:fjoldiRamma
+        if cGildi(k) <= 0
+            tStjarna(k) = 0;
+        elseif cGildi(k) >= 1
+            tStjarna(k) = 1;
         else
-            tStars(k) = Newton(@(t) adaptQuad(hradi, 0, t, quadTol) - cVals(k)*L, ...
-                               @(t) hradi(t), cVals(k), 1e-8);
+            tStjarna(k) = Newton( ...
+                @(t) adaptQuad(hradi, 0, t, quadTol) - cGildi(k)*L, ...
+                @(t) hradi(t), cGildi(k), 1e-8);
         end
     end
 
-    xBall = x(tStars);
-    yBall = y(tStars);
+    xPunktur = x(tStjarna);
+    yPunktur = y(tStjarna);
 end
 
-function animateFerill(xFull, yFull, xBall, yBall, litur, titill)
-    nFrames = length(xBall);
-    xlims = [min(xFull)-0.5, max(xFull)+0.5];
-    ylims = [min(yFull)-0.5, max(yFull)+0.5];
+
+function hreyfaFeril(xFerill, yFerill, xPunktur, yPunktur, litur, titill)
+    fjoldiRamma = length(xPunktur);
 
     figure;
-    plot(xFull, yFull, 'b-', 'LineWidth', 1.5);
-    set(gca, 'XLim', xlims, 'YLim', ylims, 'Drawmode', 'fast', 'Visible', 'on');
-    axis square;
+    plot(xFerill, yFerill, 'b-', 'LineWidth', 1.5);
+    hold on;
+    axis equal;
+    grid on;
+    xlabel('x(t)');
+    ylabel('y(t)');
 
-    ball = line('color', litur, 'Marker', 'o', 'MarkerSize', 10, ...
-                'LineWidth', 2, 'erase', 'xor', 'xdata', [], 'ydata', []);
+    punktur = plot(NaN, NaN, 'o', 'Color', litur, ...
+                   'MarkerFaceColor', litur, 'MarkerSize', 8);
 
-    for k = 1:nFrames
-        set(ball, 'xdata', xBall(k), 'ydata', yBall(k));
+    for k = 1:fjoldiRamma
+        set(punktur, 'XData', xPunktur(k), 'YData', yPunktur(k));
         if iscell(titill)
             title(titill{k});
         else
@@ -93,12 +101,29 @@ function animateFerill(xFull, yFull, xBall, yBall, litur, titill)
         pause(0.01);
     end
 end
+
+
+function I = adaptQuad(f, a, b, tol)
+    c = (a + b)/2;
+
+    heilt = (b - a) * (f(a) + f(b)) / 2;
+    vinstri = (c - a) * (f(a) + f(c)) / 2;
+    haegri = (b - c) * (f(c) + f(b)) / 2;
+
+    if abs(heilt - (vinstri + haegri)) < 3*tol
+        I = vinstri + haegri;
+    else
+        I = adaptQuad(f, a, c, tol/2) + adaptQuad(f, c, b, tol/2);
+    end
+end
+
+
 function root = Newton(f, df, x0, TOL)
     root = x0;
     for i = 1:100
         if abs(f(root)) <= TOL
             return;
         end
-        root = root - f(root)/df(root);
+        root = root - f(root) / df(root);
     end
 end
